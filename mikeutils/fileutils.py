b8cdb37bfbb36fc85901a9720d9f8c6624445022
mnolaya <michael.n.olaya@gmail.com>
2023-03-08 20:47:17 -0500

    Fixed issue with encodings between Windows/Linux

--- mikeutils/fileutils.py
+++ mikeutils/fileutils.py
@@ -1,4 +1,4 @@
-def read_file_lines(filepath, mode="rb"):
+def read_file_lines(filepath, encoding="utf-8"):
     """Open a file, read, and return list of lines containing file contents.
 
     Args:
@@ -8,5 +8,11 @@ def read_file_lines(filepath, mode="rb"):
     Returns:
         list: List containing file contents line-by-line.
     """
-    with open(filepath, mode) as f:
-        return f.readlines()
+    def readlines(f, m, e):
+        with open(f, m, encoding=e) as f:
+            return f.readlines()
+    try:
+        lines = readlines(filepath, "r", encoding)
+    except:
+        lines = [r.decode("windows-1252") for r in readlines(filepath, "rb", encoding)]
+    return lines
