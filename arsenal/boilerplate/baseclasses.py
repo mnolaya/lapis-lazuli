9b0ff84b65297dd45d36aadad78e0cde9b9556dc
Michael Olaya <michael.n.olaya@gmail.com>
2023-03-16 11:43:56 -0400

    Updated type annotations.

--- arsenal/boilerplate/baseclasses.py
+++ arsenal/boilerplate/baseclasses.py
@@ -1,13 +1,11 @@
-from typing import Callable
-
 from attrs import field
 import numpy as np
 
-def _repr_fill_dotdotdot(_) -> str:
+def _repr_fill_dotdotdot(_: str) -> str:
     """Utility function for dataclasses that replaces the default repr with '...'."""
     return "..."
 
-def np_field(repr="short") -> field:
+def np_field(repr: str = "short") -> field:
     """Utility function for dataclasses that returns a field with converter for numpy arrays."""
     kwargs = {"converter": np.array}
     if repr.lower() == "short": kwargs.append("repr": _repr_fill_dotdotdot)
