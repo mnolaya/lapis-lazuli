93f74bb2f340827019d631658bba33e4732e7d47
Michael Olaya <michael.n.olaya@gmail.com>
2023-03-16 11:47:49 -0400

    Modified np_field with the ability to add kwargs for attrs field.

--- arsenal/boilerplate/baseclasses.py
+++ arsenal/boilerplate/baseclasses.py
@@ -5,11 +5,12 @@ def _repr_fill_dotdotdot(_: str) -> str:
     """Utility function for dataclasses that replaces the default repr with '...'."""
     return "..."
 
-def np_field(repr: str = "short") -> field:
+def np_field(short_repr: bool = True, **kwargs) -> field:
     """Utility function for dataclasses that returns a field with converter for numpy arrays."""
-    kwargs = {"converter": np.array}
-    if repr.lower() == "short": kwargs.append("repr": _repr_fill_dotdotdot)
-    return field(**kwargs)
+    field_kwargs = {"converter": np.array}
+    field_kwargs.update(**kwargs)
+    if short_repr: field_kwargs.update({"repr": _repr_fill_dotdotdot})
+    return field(**field_kwargs)
 
 if __name__ == "__main__":
     from attrs import define, field
